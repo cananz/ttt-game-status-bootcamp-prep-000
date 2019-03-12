bc17475b55a0532f7564aec0c3facdf5ae5d6e50
Marisa Canan-Zucker <mcananzucker@gmail.com>
2019-03-12 01:51:57 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -50,6 +50,15 @@ end
 
 
 def winner(board)
+  WIN_COMBINATIONS.each {|win_combination|
+    index_0 = win_combination[0]
+    index_1 = win_combination[1]
+    index_2 = win_combination[2]
+
+    position_1 = board[index_0]
+    position_2 = board[index_1]
+    position_3 = board[index_2]
+ 
   if won?(board)
     return position_1
   end
